88de3ba0645401a1f2962c9ad38ad8362dbef6ca
Caroline <caroline.oh@nytimes.com>
2018-02-28 01:10:57 -0500

    mobile view updated
    scale adjusted
    navigation still needs work

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 window.onload = function(){
 
+  document.getElementById('hamburger').addEventListener('click', () => {
+    document.getElementsByTagName('nav')[0].classList.toggle('flex');
+  });
+
   var auxbout = document.getElementById('Auxbout');
-  document.getElementById('auxboutButton').addEventListener('click',scrollToAuxbout);
+  document.getElementById('auxboutButton').addEventListener('click',pressAux);
+
+  function pressAux() {
+    scrollToAuxbout();
+    auxToAuxbout();
+  }
 
   function scrollToAuxbout() {
       auxbout.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
@@ -9,7 +18,12 @@ window.onload = function(){
   }
 
   var seund = document.getElementById('Seund');
-  document.getElementById('seundButton').addEventListener('click',scrollToSeund);
+  document.getElementById('seundButton').addEventListener('click',pressSend);
+
+  function pressSend() {
+    scrollToSeund();
+    sendToSeund();
+  }
 
   function scrollToSeund() {
       seund.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
@@ -17,7 +31,12 @@ window.onload = function(){
   }
 
   var showy = document.getElementById('Showy');
-  document.getElementById('showyButton').addEventListener('click',scrollToShowy);
+  document.getElementById('showyButton').addEventListener('click',pressShitty);
+
+  function pressShitty() {
+    scrollToShowy();
+    shittyToShowy();
+  }
 
   function scrollToShowy() {
       showy.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
@@ -25,13 +44,56 @@ window.onload = function(){
   }
 
   var limits = document.getElementById('Limits');
-  document.getElementById('limitsButton').addEventListener('click',scrollToLimits);
+  document.getElementById('limitsButton').addEventListener('click',pressLimits);
+
+  function pressLimits() {
+    scrollToLimits();
+    limitsToLimits();
+  }
 
   function scrollToLimits() {
       limits.scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
       console.log('Scroll to Limits');
   }
 
+// document.getElementById('auxboutButton').addEventListener('click',auxToAuxbout);
+// document.getElementById('seundButton').addEventListener('click',sendToSeund);
+// document.getElementById('showyButton').addEventListener('click',shittyToShowy);
+// document.getElementById('limitsButton').addEventListener('click',limitsToLimits);
 
+var aux = document.getElementById('auxboutButton').innerHTML;
+var send = document.getElementById('seundButton').innerHTML;
+var shitty = document.getElementById('showyButton').innerHTML;
+
+function menuOrig () {
+  var returntoAux = aux.replace(/AUXBOUT/g, 'AUX');
+  document.getElementById('auxboutButton').innerHTML = returntoAux;
+  var returntoSend = send.replace(/SEUND/g, 'SEND');
+  document.getElementById('seundButton').innerHTML = returntoSend;
+  var returntoShitty = shitty.replace(/SHOWY/g, 'SHITTY');
+  document.getElementById('showyButton').innerHTML = returntoShitty;
+}
+
+function auxToAuxbout() {
+  menuOrig();
+  var changetoAuxbout = aux.replace(/AUX/g, 'AUXBOUT');
+  document.getElementById('auxboutButton').innerHTML = changetoAuxbout;
+}
+
+function sendToSeund() {
+  menuOrig();
+  var changetoSeund = send.replace(/SEND/g, 'SEUND');
+  document.getElementById('seundButton').innerHTML = changetoSeund;
+}
+
+function shittyToShowy() {
+  menuOrig();
+  var changetoShowy = shitty.replace(/SHITTY/g, 'SHOWY');
+  document.getElementById('showyButton').innerHTML = changetoShowy;
+}
+
+function limitsToLimits() {
+  menuOrig();
+}
 
 }
